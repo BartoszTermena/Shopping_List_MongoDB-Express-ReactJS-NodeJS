af34b15b0e14a06d6e767fdd0b4af1246f75fc83
bartek <Bartek.NNAAN@wp.pl>
2019-01-16 19:58:57 +0100

    created shopping list project

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://Bartek:test12345@ds259154.mlab.com:59154/shopping_list'
+}
---
+++ routes/api/items.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+
+const Item = require('../../models/Item');
+
+//GET api/items
+//@access Public
+router.get('/', (req, res) => {
+    Item.find()
+    .sort({ date: -1 })
+    .then(items => {
+        res.json(items);
+    })
+    .catch(err => {
+        res.status(404).json({message: err});
+    });
+});
+//POST api/items
+//@access Public
+router.post('/', (req, res) => {
+    const newItem = new Item({
+        name: req.body.name
+    });
+
+    newItem.save()
+    .then(item => {
+        res.json(item);
+    })
+    .catch(err => {
+        res.status(404).json({message: err});
+    });
+});
+//DELETE api/items/:id
+//@access Public
+router.delete('/:id', (req, res) => {
+    Item.findById(req.params.id)
+    .then(item => {
+        item.remove().then(() => {
+            res.json({message: 'item removed'});
+        });
+    })
+    .catch(err => {
+        res.status(404).json({message: err});
+    });
+});
+
+
+module.exports = router;

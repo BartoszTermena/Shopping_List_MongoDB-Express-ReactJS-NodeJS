65e2dcb8a64fce8576e15ec55f425c7e3b219282
bartek <Bartek.NNAAN@wp.pl>
2019-01-24 13:54:50 +0100

    ASD

--- server.js
+++ server.js
@@ -24,15 +24,16 @@ mongoose.connect(db)
 //use routes
 app.use('/api/items', items);
 
-//server static assets if in production
-if(process.env.NODE_ENV === 'production') {
-    //set static folder
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
     app.use(express.static('client/build'));
-
+  
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
-}
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+  
 
 const port = process.env.PORT || 5000;
 
